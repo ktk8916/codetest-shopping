b0b752a658114ade37ccf597a5a2b5254590dc56
ktk8916 <ktk8916@naver.com>
2023-12-14 17:09:46 +0900

    user name unique key 추가

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -1,6 +1,9 @@
+-- table history
+
 create table member(
     id int auto_increment primary key,
     username varchar(255),
     password varchar(255),
-    nickname varchar(255)
+    nickname varchar(255),
+    unique uk_username(username)
 );
