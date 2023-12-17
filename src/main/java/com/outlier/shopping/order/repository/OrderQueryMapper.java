73e1543edb1a2bb56ef1e65df7218420b3ae8796
ktk8916 <ktk8916@naver.com>
2023-12-17 21:15:29 +0900

    주문 내역 조회 기능 추가

--- src/main/java/com/outlier/shopping/order/repository/OrderQueryMapper.java
+++ src/main/java/com/outlier/shopping/order/repository/OrderQueryMapper.java
@@ -1,7 +1,13 @@
 package com.outlier.shopping.order.repository;
 
+import com.outlier.shopping.order.domain.dto.OrderReceiptDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface OrderQueryMapper {
+
+    List<OrderReceiptDto> findReceiptByMemberIdAndCondition(Long memberId, int limit, int offset);
+    int findReceiptTotalSizeByCondition(Long memberId);
 }
--- src/main/java/com/outlier/shopping/order/service/OrderService.java
+++ src/main/java/com/outlier/shopping/order/service/OrderService.java
@@ -3,11 +3,14 @@ package com.outlier.shopping.order.service;
 import com.outlier.shopping.cart.domain.entity.CartItem;
 import com.outlier.shopping.cart.repository.CartMapper;
 import com.outlier.shopping.member.domain.entity.Member;
+import com.outlier.shopping.order.domain.dto.OrderReceiptDto;
 import com.outlier.shopping.order.domain.entity.Order;
 import com.outlier.shopping.order.domain.entity.OrderItem;
 import com.outlier.shopping.order.domain.response.BillResponse;
+import com.outlier.shopping.order.domain.response.OrderHistoryResponse;
 import com.outlier.shopping.order.repository.OrderItemMapper;
 import com.outlier.shopping.order.repository.OrderMapper;
+import com.outlier.shopping.order.repository.OrderQueryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,7 @@ public class OrderService {
 
     private final OrderMapper orderMapper;
     private final OrderItemMapper orderItemMapper;
+    private final OrderQueryMapper orderQueryMapper;
     private final CartMapper cartMapper;
 
     @Transactional
@@ -50,4 +54,10 @@ public class OrderService {
         return BillResponse.of(order.getId(), totalPrice,order.getCreatedAt());
     }
 
+    public OrderHistoryResponse getMyOrderDetails(Long memberId, int page, int size) {
+        int offset = page * size;
+        List<OrderReceiptDto> orders = orderQueryMapper.findReceiptByMemberIdAndCondition(memberId, size, offset);
+        int totalSize = orderQueryMapper.findReceiptTotalSizeByCondition(memberId);
+        return OrderHistoryResponse.of(orders, page, size, totalSize);
+    }
 }
