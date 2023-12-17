5e899fad9c4b79993a6a080218b1c5e1871d6b52
ktk8916 <ktk8916@naver.com>
2023-12-17 22:50:52 +0900

    validation 추가

--- src/main/java/com/outlier/shopping/auth/controller/AuthController.java
+++ src/main/java/com/outlier/shopping/auth/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.outlier.shopping.auth.domain.request.LoginRequest;
 import com.outlier.shopping.auth.domain.request.SignupRequest;
 import com.outlier.shopping.auth.domain.response.LoginResponse;
 import com.outlier.shopping.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,12 @@ public class AuthController {
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public LoginResponse signup(@RequestBody SignupRequest request){
+    public LoginResponse signup(@RequestBody @Valid SignupRequest request){
         return authService.signup(request);
     }
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginRequest request){
+    public LoginResponse login(@RequestBody @Valid LoginRequest request){
         return authService.login(request);
     }
 }
