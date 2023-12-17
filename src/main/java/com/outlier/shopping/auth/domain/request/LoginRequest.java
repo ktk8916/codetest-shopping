6e43c7c1b046d736c766fb33d8aec34580f45d34
ktk8916 <ktk8916@naver.com>
2023-12-17 22:50:46 +0900

    validation 추가

--- src/main/java/com/outlier/shopping/auth/domain/request/LoginRequest.java
+++ src/main/java/com/outlier/shopping/auth/domain/request/LoginRequest.java
@@ -1,7 +1,11 @@
 package com.outlier.shopping.auth.domain.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+        @NotBlank(message = "username is required")
         String username,
+        @NotBlank(message = "password is required")
         String password
 ) {
 }
