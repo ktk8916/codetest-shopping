7decfecf1dcfd803ee637e5b56e92d59efc94451
ktk8916 <ktk8916@naver.com>
2023-12-17 21:27:25 +0900

    전역 예외처리 추가

--- src/main/java/com/outlier/shopping/global/exception/CustomRestControllerAdvice.java
+++ src/main/java/com/outlier/shopping/global/exception/CustomRestControllerAdvice.java
@@ -1,11 +1,22 @@
 package com.outlier.shopping.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class CustomRestControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException e){
+        log.error("unknown server error", e);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, "unknown server error"));
+    }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ExceptionResponse> handleCustomException(CustomException e){
