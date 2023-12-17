64ee770482ac8fcf15cd64d48f46badfddaef2b7
ktk8916 <ktk8916@naver.com>
2023-12-17 13:18:54 +0900

    생성 메서드 추가

--- src/main/java/com/outlier/shopping/product/domain/entity/Product.java
+++ src/main/java/com/outlier/shopping/product/domain/entity/Product.java
@@ -11,6 +11,14 @@ public class Product {
     private int price;
     private Long managerId; // 상품을 등록한 사람
 
+    public static Product createProduct(String name, int price, Long managerId){
+        return Product.builder()
+                .name(name)
+                .price(price)
+                .managerId(managerId)
+                .build();
+    }
+
     @Builder
     public Product(String name, int price, Long managerId) {
         this.name = name;
