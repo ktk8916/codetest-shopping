958c748175fbec4adcdefdf1e4c93c56d1acc77e
ktk8916 <ktk8916@naver.com>
2023-12-18 00:19:12 +0900

    테스트 프로파일 설정

--- src/test/java/com/outlier/shopping/ShoppingApplicationTests.java
+++ src/test/java/com/outlier/shopping/ShoppingApplicationTests.java
@@ -2,8 +2,10 @@ package com.outlier.shopping;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ShoppingApplicationTests {
 
     @Test
