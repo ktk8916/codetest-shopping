9aaaff31cb82a6a45ddbeb8ed04a85b60c798278
ktk8916 <ktk8916@naver.com>
2023-12-17 23:17:03 +0900

    validation 전역 예외처리 추가

--- src/main/java/com/outlier/shopping/global/exception/CustomRestControllerAdvice.java
+++ src/main/java/com/outlier/shopping/global/exception/CustomRestControllerAdvice.java
@@ -3,10 +3,12 @@ package com.outlier.shopping.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 @Slf4j
@@ -17,6 +19,18 @@ public class CustomRestControllerAdvice {
         FieldError fieldError = e.getBindingResult().getFieldErrors().get(0);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ExceptionResponse.of(HttpStatus.BAD_REQUEST, fieldError.getDefaultMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionResponse.of(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionResponse.of(HttpStatus.BAD_REQUEST, e.getName() + " argument type miss match"));
     }
 
     @ExceptionHandler(CustomException.class)
