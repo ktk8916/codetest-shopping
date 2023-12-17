18b66fb3cc89a5c11d43f36281d773a5c3eac0f2
ktk8916 <ktk8916@naver.com>
2023-12-17 22:51:03 +0900

    validation 전역 예외 처리 추가

--- src/main/java/com/outlier/shopping/global/exception/CustomRestControllerAdvice.java
+++ src/main/java/com/outlier/shopping/global/exception/CustomRestControllerAdvice.java
@@ -3,12 +3,27 @@ package com.outlier.shopping.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class CustomRestControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        FieldError fieldError = e.getBindingResult().getFieldErrors().get(0);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionResponse.of(HttpStatus.BAD_REQUEST, fieldError.getDefaultMessage()));
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionResponse> handleCustomException(CustomException e){
+        return ResponseEntity.status(e.getHttpStatus())
+                .body(ExceptionResponse.of(e.getHttpStatus(), e.getMessage()));
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException e){
@@ -18,9 +33,4 @@ public class CustomRestControllerAdvice {
                 .body(ExceptionResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, "unknown server error"));
     }
 
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ExceptionResponse> handleCustomException(CustomException e){
-        return ResponseEntity.status(e.getHttpStatus())
-                .body(ExceptionResponse.of(e.getHttpStatus(), e.getMessage()));
-    }
 }
