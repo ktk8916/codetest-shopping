439a21ac3ce0d358070506650175ec28b7ffe688
ktk8916 <ktk8916@naver.com>
2023-12-17 16:03:07 +0900

    생성 메서드 추가

--- src/main/java/com/outlier/shopping/cart/domain/entity/CartItem.java
+++ src/main/java/com/outlier/shopping/cart/domain/entity/CartItem.java
@@ -1,20 +1,33 @@
 package com.outlier.shopping.cart.domain.entity;
 
+import com.outlier.shopping.member.domain.entity.Member;
+import com.outlier.shopping.product.domain.entity.Product;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CartItem {
 
     private Long id;
-    private Long memberId;
-    private Long productId;
+    private Member member;
+    private Product product;
     private int quantity;
 
+    public static CartItem createCartItem(Member member, Product product, int quantity){
+        return CartItem.builder()
+                .member(member)
+                .product(product)
+                .quantity(quantity)
+                .build();
+    }
+
     @Builder
-    public CartItem(Long memberId, Long productId, int quantity) {
-        this.memberId = memberId;
-        this.productId = productId;
+    public CartItem(Member member, Product product, int quantity) {
+        this.member = member;
+        this.product = product;
         this.quantity = quantity;
     }
 }
